82d2e12d839f762c395eeed164b463e74f4a8cc3
Shlomi Fish <shlomif@shlomifish.org>
2013-01-20 20:06:16 +0200

    Add more.

--- App-Mousable/lib/App/Mousable.pm
+++ App-Mousable/lib/App/Mousable.pm
@@ -3,5 +3,12 @@ package App::Mousable;
 use strict;
 use warnings;
 
+our $VERSION = '0.0.1';
+
 1;
+
+=head1 NAME
+
+App::Mousable - the main dancer application implementing an AJAX-based
+commenting engine.
 
